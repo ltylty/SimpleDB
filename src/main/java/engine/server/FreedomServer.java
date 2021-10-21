1a00bcdedaa03cba160a5340b8d985e5d742ad6a
liutianyuan <liutianyuan1737@stnts.com>
2021-10-21 11:10:11 +0800

    format

--- src/main/java/engine/server/FreedomServer.java
+++ src/main/java/engine/server/FreedomServer.java
@@ -58,12 +58,13 @@ public class FreedomServer extends Thread {
             // 这边的childHandler是用来管理accept的
             // 由于线程间传递的是byte[],所以内存池okay
             // 只需要保证分配ByteBuf和write在同一个线程(函数)就行了
-            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 1024)
-                    .childHandler(new FrontHandlerFactory()).option(ChannelOption.ALLOCATOR,
-                    PooledByteBufAllocator.DEFAULT)
-                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, SocketConfig.CONNECT_TIMEOUT_MILLIS)
-                    .option(ChannelOption.SO_TIMEOUT, SocketConfig.SO_TIMEOUT);
+            b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new FrontHandlerFactory())
+                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, SocketConfig.CONNECT_TIMEOUT_MILLIS)
+                .option(ChannelOption.SO_TIMEOUT, SocketConfig.SO_TIMEOUT);
             ChannelFuture f = b.bind(databaseInstance.getServerPort()).sync();
             f.channel().closeFuture().sync();
 
